aab4d57bf791d5fc6f19f4b002f50dde3d233a1e
MaheenHabib <maheenhabib05@gmail.com>
2024-12-05 00:47:13 +0500

    DONE

---
+++ gender.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char gender;
+	
+	cout<<"Enter a single letter ('f' for female or any other letter for male):"<<endl;
+	cin>>gender;
+	if (gender == 'f'|| gender== 'f') {
+		cout<<"you are a female"<<endl;
+	}
+	else {
+		cout<<"you are a male"<< endl;
+	}
+	return 0;
+}
